aba61a4e219842f8f088a9cdaa40de1e8c4333eb
ErickZCortes <Erickccortes@gmail.com>
2018-11-06 17:26:23 -0600

    06/11/18

--- RestaurantApp/src/app/app.module.ts
+++ RestaurantApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -30,7 +30,8 @@ import { RecipesEditComponent } from './recipes/recipes-edit/recipes-edit.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [IngredientsService],
   bootstrap: [AppComponent]
